e132e8189601f704a08a034eabf579df41db8b2a
teodora.jovcheska <teodora.jovcheska@testdevlab.com>
2022-03-30 13:11:35 +0200

    Added shell script.

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
-python --version
-pip install virtualenv
-pwd
-pip install -r requirements.txt
-python main.py
+PATH=$PATH:/Users/teodorajovcheska/.pyenv/bin:/Users/teodorajovcheska/.pyenv/shims:/Users/teodorajovcheska/.pyenv/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+echo $PATH
+python3 --version
+pip3 --version
+pip3 install virtualenv
+virtualenv --version
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+deactivate
