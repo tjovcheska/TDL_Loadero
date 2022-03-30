85e2437c9aea142d35d8cc5d71f0734d961ab992
teodora.jovcheska <teodora.jovcheska@testdevlab.com>
2022-03-30 13:33:37 +0200

    Updated shell script.

--- script.sh
+++ script.sh
@@ -1,10 +1,6 @@
-PATH=$PATH:/Users/teodorajovcheska/.pyenv/bin:/Users/teodorajovcheska/.pyenv/shims:/Users/teodorajovcheska/.pyenv/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-echo $PATH
-python3 --version
-pip3 --version
 pip3 install virtualenv
 virtualenv --version
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-deactivate
+python3 main.py
